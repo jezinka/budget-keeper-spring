b196f8369ed2881d96e4339e54ef6e4bbed2cfb0
jezinka <jezina1989@gmail.com>
2022-09-29 16:42:39 +0200

    clean up code

--- src/main/java/com/example/budgetkeeperspring/category/CategoryRepository.java
+++ src/main/java/com/example/budgetkeeperspring/category/CategoryRepository.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -14,7 +13,6 @@ public class CategoryRepository {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    @GetMapping("")
     public List<Category> getAll() {
         return jdbcTemplate.query("select id, name from category", BeanPropertyRowMapper.newInstance(Category.class));
     }
--- src/main/java/com/example/budgetkeeperspring/moneyAmount/MoneyAmountController.java
+++ src/main/java/com/example/budgetkeeperspring/moneyAmount/MoneyAmountController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("moneyAmount")
 public class MoneyAmountController {
