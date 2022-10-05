0b852ec7d1955993f8f8e9d111d08ca47cc053b5
jezinka <jezina1989@gmail.com>
2022-10-05 12:50:15 +0200

    jdbc password storage

--- src/main/java/com/example/budgetkeeperspring/SpringSecurityConfigurationBasicAuth.java
+++ src/main/java/com/example/budgetkeeperspring/SpringSecurityConfigurationBasicAuth.java
@@ -1,14 +1,22 @@
 package com.example.budgetkeeperspring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,5 +27,16 @@ public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerA
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .usersByUsernameQuery("select username, password, enabled from users where username=?")
+                .authoritiesByUsernameQuery("select username, role from users where username=?")
+
+        ;
     }
 }
