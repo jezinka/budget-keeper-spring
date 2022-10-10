713dea37b80ffccd78c342d2c68b4415370cb915
jezinka <jezina1989@gmail.com>
2022-10-10 14:02:04 +0200

    get rid of unnecessary mapper class

--- src/main/java/com/example/budgetkeeperspring/moneyAmount/MoneyAmountRepository.java
+++ src/main/java/com/example/budgetkeeperspring/moneyAmount/MoneyAmountRepository.java
@@ -1,14 +1,12 @@
 package com.example.budgetkeeperspring.moneyAmount;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
 
@@ -37,20 +35,8 @@ public class MoneyAmountRepository {
                         "      from transaction t " +
                         "      where transaction_date between cast(:begin AS DATE) and cast(:end AS DATE)) balance " +
                         "         join money_amount " +
-                        "where month between cast(:begin AS DATE) and cast(:end AS DATE);", namedParameters, new MoneyAmountRowMapper());
-    }
-}
-
-class MoneyAmountRowMapper implements RowMapper<MoneyAmount> {
-    @Override
-    public MoneyAmount mapRow(ResultSet rs, int rowNum) throws SQLException {
-        MoneyAmount moneyAmount = new MoneyAmount();
-
-        moneyAmount.setAmount(rs.getFloat("AMOUNT"));
-        moneyAmount.setIncome(rs.getFloat("INCOME"));
-        moneyAmount.setOutcome(rs.getFloat("OUTCOME"));
-        moneyAmount.setAccountBalance(rs.getFloat("ACCOUNT_BALANCE"));
-
-        return moneyAmount;
+                        "where month between cast(:begin AS DATE) and cast(:end AS DATE);",
+                namedParameters,
+                BeanPropertyRowMapper.newInstance(MoneyAmount.class));
     }
 }
