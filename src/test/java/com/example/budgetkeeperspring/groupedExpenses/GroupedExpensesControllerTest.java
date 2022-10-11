8024e3eb048a7fa87544885e33257eed489186b4
jezinka <jezina1989@gmail.com>
2022-10-11 09:17:08 +0200

    test for grouped expenses

--- src/test/java/com/example/budgetkeeperspring/groupedExpenses/GroupedExpensesControllerTest.java
+++ src/test/java/com/example/budgetkeeperspring/groupedExpenses/GroupedExpensesControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.time.LocalDate;
 import java.util.List;
 
+import static java.time.Month.JANUARY;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -62,5 +63,30 @@ class GroupedExpensesControllerTest {
         assertNotNull(transactionList.stream().filter(t -> t.category.equals("A") && t.month == currentMonth && t.amount == -43.28).findFirst());
         assertNotNull(transactionList.stream().filter(t -> t.category.equals("SUMA") && t.amount == -43.28).findFirst());
         assertNotNull(transactionList.stream().filter(t -> t.month == 99 && t.amount == -43.28).findFirst());
+    }
+
+    @Test
+    void getForCurrentYear_3_transactions() {
+        // given:
+        prepare();
+        int currentMonth = LocalDate.now().getMonthValue();
+        String op = currentMonth == JANUARY.getValue() ? "DATE_ADD" : "DATE_SUB";
+
+        jdbcTemplate.execute("INSERT INTO category (id, name) VALUES (1, 'A');");
+        jdbcTemplate.execute("INSERT INTO category (id, name) VALUES (2, 'B');");
+        jdbcTemplate.execute("INSERT INTO transaction (id, transaction_date, title, payee, amount, category_id) VALUES (1, " + op + "(now(), INTERVAL 1 MONTH), 'BLIK', '', -5, 1);");
+        jdbcTemplate.execute("INSERT INTO transaction (id, transaction_date, title, payee, amount, category_id) VALUES (3, now(), 'BLIK', '', -10, 1);");
+        jdbcTemplate.execute("INSERT INTO transaction (id, transaction_date, title, payee, amount, category_id) VALUES (2, now(), 'BLIK', '', -20, 2);");
+
+        // when:
+        List<GroupedExpenses> transactionList = controller.getForCurrentYear();
+
+        // then:
+        assertEquals(7, transactionList.size());
+        assertNotNull(transactionList.stream().filter(t -> t.category.equals("SUMA") && t.month == currentMonth && t.amount == -30).findFirst());
+        assertNotNull(transactionList.stream().filter(t -> t.category.equals("SUMA") && t.month != currentMonth && t.amount == -5).findFirst());
+        assertNotNull(transactionList.stream().filter(t -> t.category.equals("A") && t.month == 99 && t.amount == -15).findFirst());
+        assertNotNull(transactionList.stream().filter(t -> t.category.equals("B") && t.month == 99 && t.amount == -20).findFirst());
+
     }
 }
