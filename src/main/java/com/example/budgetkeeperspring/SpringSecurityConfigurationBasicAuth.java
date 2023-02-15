8e8c785348a4c5710b8934c55ff1299f988d69df
jezinka <jezina1989@gmail.com>
2023-02-15 17:27:10 +0100

    disable csrf

--- src/main/java/com/example/budgetkeeperspring/SpringSecurityConfigurationBasicAuth.java
+++ src/main/java/com/example/budgetkeeperspring/SpringSecurityConfigurationBasicAuth.java
@@ -21,6 +21,7 @@ public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerA
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest().authenticated()
