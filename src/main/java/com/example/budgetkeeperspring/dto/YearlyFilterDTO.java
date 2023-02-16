7255411fa5ca6cc9b5d839ba9fba327b66f7d0ee
jezinka <jezina1989@gmail.com>
2023-02-16 15:30:20 +0100

    remove component annotation from DTO

--- src/main/java/com/example/budgetkeeperspring/dto/YearlyFilterDTO.java
+++ src/main/java/com/example/budgetkeeperspring/dto/YearlyFilterDTO.java
@@ -3,12 +3,10 @@ package com.example.budgetkeeperspring.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Component
 public class YearlyFilterDTO {
     Integer year;
 }
