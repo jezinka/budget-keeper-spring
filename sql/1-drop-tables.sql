bebbfce47299f807d5e75c1b2c64070a2aaf0550
jezinka <jezina1989@gmail.com>
2023-06-11 09:59:06 +0200

    remove bank sql script for cleaning up

---
+++ sql/1-drop-tables.sql
@@ -0,0 +1,6 @@
+drop table liability_lookout;
+drop table liability;
+drop table bank;
+drop table bank_seq;
+drop table liability_lookout_seq;
+drop table liability_seq;
--- src/main/java/com/example/budgetkeeperspring/repository/BankRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.budgetkeeperspring.repository;
-
-import com.example.budgetkeeperspring.entity.Bank;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankRepository extends JpaRepository<Bank, Long> {
-}
