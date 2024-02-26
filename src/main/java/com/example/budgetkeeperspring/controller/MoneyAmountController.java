323db5bcf5e74403f1e081e7949cf4a3130567bc
jezinka <jezina1989@gmail.com>
2024-02-26 19:04:44 +0100

    endpoint for selected period of time

--- src/main/java/com/example/budgetkeeperspring/controller/MoneyAmountController.java
+++ src/main/java/com/example/budgetkeeperspring/controller/MoneyAmountController.java
@@ -9,10 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
@@ -35,12 +32,18 @@ public class MoneyAmountController {
         return moneyAmountService.getForPeriod(startDate, endDate);
     }
 
+    //get for period from request params
+    @GetMapping(MONEY_AMOUNT_PATH + "/period")
+    CurrentMonthMoneyAmountDTO getForPeriod(@RequestParam("startDate") LocalDate startDate, @RequestParam("endDate") LocalDate endDate) {
+        return moneyAmountService.getForPeriod(startDate, endDate);
+    }
+
     @PostMapping(MONEY_AMOUNT_PATH)
     ResponseEntity<MoneyAmountDTO> addMoneyAmountForCurrentMonth(@Validated @RequestBody MoneyAmountDTO newAmount) {
         MoneyAmountDTO savedMoneyAmount = moneyAmountService.addMoneyAmountForCurrentMonth(newAmount);
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location", MONEY_AMOUNT_PATH + "/" + savedMoneyAmount.getId().toString());
-        return new ResponseEntity(headers, HttpStatus.CREATED);
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
 
     }
 }
