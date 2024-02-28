f353870eb02c15dc9919c0f990081003d1102694
jezinka <jezina1989@gmail.com>
2024-02-28 16:16:02 +0100

    reduce chart from 31 to the current month duration

--- app/src/components/currentMonth/DailyBarChart.js
+++ app/src/components/currentMonth/DailyBarChart.js
@@ -14,11 +14,14 @@ const DailyBarChart = () => {
         setData(data);
     }
 
+    // last day of current month
+    const lastDay = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate();
 
     return (
         <BarChart width={500} height={500} data={data} layout="vertical" className={"mt-3"}>
             <XAxis type="number" domain={[0, 500]}/>
-            <YAxis dataKey="day" type="number" domain={[1, 31]} interval={0} tickCount={32}/>
+
+            <YAxis dataKey="day" type="number" domain={[1, lastDay]} interval={0} tickCount={lastDay + 1}/>
             <Bar dataKey="amount" fill="#6BBDFF">
                 <LabelList dataKey="amount" position="right"/>
             </Bar>
