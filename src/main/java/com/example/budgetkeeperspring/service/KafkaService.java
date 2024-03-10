759acbd8bd14bda973eb2d4db3a02fd7b9d2de12
jezinka <jezina1989@gmail.com>
2024-03-10 19:21:12 +0100

    add logs

--- src/main/java/com/example/budgetkeeperspring/service/KafkaService.java
+++ src/main/java/com/example/budgetkeeperspring/service/KafkaService.java
@@ -7,11 +7,13 @@ import com.example.budgetkeeperspring.mapper.LogMapper;
 import com.example.budgetkeeperspring.repository.LogRepository;
 import com.google.gson.Gson;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class KafkaService {
@@ -24,6 +26,7 @@ public class KafkaService {
 
     @KafkaListener(id = "springExpenseListener", topics = "expense")
     public void listenExpenses(String in) {
+        log.info("Received message: " + in);
         Gson g = new Gson();
         ExpenseDTO message = g.fromJson(in, ExpenseDTO.class);
         Optional<Category> category = categoryService.findCategoryByConditions(message);
@@ -31,12 +34,15 @@ public class KafkaService {
         if (savedExpense != null) {
             fixedCostService.updateFixedCost(savedExpense);
         }
+        log.info("Saved expense: " + savedExpense);
     }
 
-        @KafkaListener(id = "springLogListener", topics = "log")
+    @KafkaListener(id = "springLogListener", topics = "log")
     public void listenLogs(String in) {
+        log.info("Received message: " + in);
         Gson g = new Gson();
-        LogDTO log = g.fromJson(in, LogDTO.class);
-        logRepository.save(logMapper.mapToEntity(log));
+        LogDTO logDto = g.fromJson(in, LogDTO.class);
+        logRepository.save(logMapper.mapToEntity(logDto));
+        log.info("Saved log: " + logDto);
     }
 }
