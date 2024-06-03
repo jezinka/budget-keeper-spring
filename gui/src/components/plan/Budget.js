c7e2bc1d837c47f1647c13ed96a852ce83ba6060
jezinka <jezina1989@gmail.com>
2024-06-03 17:35:40 +0200

    add modal with transaction to plan

--- gui/src/components/plan/Budget.js
+++ gui/src/components/plan/Budget.js
@@ -1,11 +1,17 @@
 import React, {useEffect, useState} from 'react';
 import Table from "react-bootstrap/Table";
-import {Col} from "react-bootstrap";
+import {Col, Modal} from "react-bootstrap";
 import BudgetSummary from "./BudgetSummary";
 import {formatNumber} from "../../Utils";
+import Expense from "../year/Expense";
 
 export default function Budget() {
     const [budgetPlan, setBudgetPlan] = useState([]);
+    const [show, setShow] = useState(false);
+    const [transactionsDetails, setTransactionsDetails] = useState([]);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     async function loadData() {
         const response = await fetch('/budget/budgetPlan')
@@ -25,55 +31,70 @@ export default function Budget() {
         return budgetPlan.filter(g => g.goal !== 0).map(row => row.difference).reduce((sum, num) => sum + num, 0);
     }
 
-    return (
-        <>
-            <Col sm={5}>
-                <h5>PLAN</h5>
-                <Table id='budgetPlanTable' responsive='sm' striped bordered size="sm">
-                    <tbody>
-                    <tr className='table-info'>
-                        <td>KATEGORIA</td>
-                        <td>ZAŁOŻENIE</td>
-                        <td>WYDANE</td>
-                        <td>RÓŻNICA</td>
-                    </tr>
-                    {budgetPlan.filter(g => g.goal !== 0).map(row => <tr key={row.id}>
-                        <td>{row.category}</td>
-                        <td>{row.goal}</td>
-                        <td>{row.expense}</td>
-                        <td className={row.percentage > 0 ? (row.percentage > 100 ? 'failed_goal' : 'over_goal') : 'success_goal'}>{row.difference}</td>
-                    </tr>)}
-                    <tr>
-                        <td>RAZEM</td>
-                        <td>{getGoalTotal()}</td>
-                        <td>{budgetPlan.filter(g => g.goal !== 0).map(row => row.expense).reduce((sum, num) => sum + num, 0)}</td>
-                        <td>{formatNumber(getTotalExpenses())}</td>
-                    </tr>
-                    </tbody>
-                </Table>
-            </Col>
-            <Col sm={5}>
-                <h5>NO BUY</h5>
-                <Table id='noBuyTable' responsive='sm' striped bordered size="sm">
-                    <tbody>
-                    <tr className='table-info'>
-                        <td>KATEGORIA</td>
-                        <td>WYDANE</td>
-                    </tr>
-                    {budgetPlan.filter(g => g.goal === 0).map(row => <tr key={row.id}>
-                        <td>{row.category}</td>
-                        <td className={row.expense === 0 ? 'success_goal' : 'failed_goal'}>{row.expense}</td>
-                    </tr>)}
-                    <tr>
-                        <td>RAZEM</td>
-                        <td>{formatNumber(budgetPlan.filter(g => g.goal === 0).map(row => row.expense).reduce((sum, num) => sum + num, 0))}</td>
-                    </tr>
-                    </tbody>
-                </Table>
-            </Col>
-            <Col>
-                <h5>PODSUMOWANIE</h5>
-                <BudgetSummary/>
-            </Col>
-        </>);
+    let currentMonth = new Date().getMonth() + 1;
+    let currentYear = new Date().getFullYear();
+
+    function getExpense(row) {
+        return <Expense
+            expense={{month: currentMonth, category: row.category, amount: row.expense}}
+            year={currentYear}
+            key={currentMonth + row.category}
+            modalHandler={handleShow}
+            modalContentHandler={setTransactionsDetails}/>;
+    }
+
+    return (<>
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton> <Modal.Title>Transakcje</Modal.Title> </Modal.Header>
+            <Modal.Body>{transactionsDetails}</Modal.Body>
+        </Modal>
+        <Col sm={5}>
+            <h5>PLAN</h5>
+            <Table id='budgetPlanTable' responsive='sm' striped bordered size="sm">
+                <tbody>
+                <tr className='table-info'>
+                    <td>KATEGORIA</td>
+                    <td>ZAŁOŻENIE</td>
+                    <td>WYDANE</td>
+                    <td>RÓŻNICA</td>
+                </tr>
+                {budgetPlan.filter(g => g.goal !== 0).map(row => <tr key={row.id}>
+                    <td>{row.category}</td>
+                    <td>{row.goal}</td>
+                    <td>{getExpense(row)}</td>
+                    <td className={row.percentage > 0 ? (row.percentage > 100 ? 'failed_goal' : 'over_goal') : 'success_goal'}>{row.difference}</td>
+                </tr>)}
+                <tr>
+                    <td>RAZEM</td>
+                    <td>{getGoalTotal()}</td>
+                    <td>{budgetPlan.filter(g => g.goal !== 0).map(row => row.expense).reduce((sum, num) => sum + num, 0)}</td>
+                    <td>{formatNumber(getTotalExpenses())}</td>
+                </tr>
+                </tbody>
+            </Table>
+        </Col>
+        <Col sm={5}>
+            <h5>NO BUY</h5>
+            <Table id='noBuyTable' responsive='sm' striped bordered size="sm">
+                <tbody>
+                <tr className='table-info'>
+                    <td>KATEGORIA</td>
+                    <td>WYDANE</td>
+                </tr>
+                {budgetPlan.filter(g => g.goal === 0).map(row => <tr key={row.id}>
+                    <td>{row.category}</td>
+                    <td className={row.expense === 0 ? 'success_goal' : 'failed_goal'}>{getExpense(row)}</td>
+                </tr>)}
+                <tr>
+                    <td>RAZEM</td>
+                    <td>{formatNumber(budgetPlan.filter(g => g.goal === 0).map(row => row.expense).reduce((sum, num) => sum + num, 0))}</td>
+                </tr>
+                </tbody>
+            </Table>
+        </Col>
+        <Col>
+            <h5>PODSUMOWANIE</h5>
+            <BudgetSummary/>
+        </Col>
+    </>);
 }
