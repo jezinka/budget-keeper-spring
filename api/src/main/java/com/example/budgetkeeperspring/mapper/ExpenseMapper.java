2a12ea6c9f62f28b4df1f7ee8f9be65dae9cdd71
jezinka <jezina1989@gmail.com>
2024-06-06 13:12:11 +0200

    mapper with two different date formats

--- api/src/main/java/com/example/budgetkeeperspring/mapper/ExpenseMapper.java
+++ api/src/main/java/com/example/budgetkeeperspring/mapper/ExpenseMapper.java
@@ -5,13 +5,17 @@ import com.example.budgetkeeperspring.entity.Expense;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 @Mapper(componentModel = "spring")
 public interface ExpenseMapper {
 
     @Mapping(source = "deleted", target = "deleted", defaultValue = "false")
     @Mapping(source = "categoryId", target = "category.id")
     @Mapping(source = "categoryName", target = "category.name")
-    @Mapping(target = "transactionDate", dateFormat = "yyyy-MM-dd")
+    @Mapping(source = "transactionDate", target = "transactionDate")
     Expense mapToEntity(ExpenseDTO expenseDTO);
 
     @Mapping(source = "deleted", target = "deleted", defaultValue = "false")
@@ -19,4 +23,13 @@ public interface ExpenseMapper {
     @Mapping(source = "category.name", target = "categoryName")
     ExpenseDTO mapToDto(Expense expense);
 
+    default LocalDate stringToLocalDate(String date) {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            return LocalDate.parse(date, formatter);
+        } catch (DateTimeParseException e) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            return LocalDate.parse(date, formatter);
+        }
+    }
 }
