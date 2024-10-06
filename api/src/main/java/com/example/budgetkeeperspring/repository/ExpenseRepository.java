44a692d85dd7e642cb344b8158670c88e2c5369d
jezinka <jezina1989@gmail.com>
2024-10-06 10:37:31 +0200

    FIRE number without mortgage

--- api/src/main/java/com/example/budgetkeeperspring/repository/ExpenseRepository.java
+++ api/src/main/java/com/example/budgetkeeperspring/repository/ExpenseRepository.java
@@ -25,7 +25,7 @@ public interface ExpenseRepository extends JpaRepository<Expense, Long> {
     @Query("select new com.example.budgetkeeperspring.dto.YearlyExpensesDTO(year(e.transactionDate), sum(e.amount)) " +
            "from Expense e " +
            "left join fetch Category c " +
-           "where (c.level is null or c.level not in (2, 4)) " +
+           "where (c.level is null or c.level not in (2, 4, 5)) " +
            "              and year(e.transactionDate) <= (year(curdate()) - 1) " +
            "group by year(e.transactionDate)")
     List<YearlyExpensesDTO> findAnnualExpensesForPreviousYears();
