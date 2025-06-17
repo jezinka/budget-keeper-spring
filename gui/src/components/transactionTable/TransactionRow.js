110137b3829fca75f4a25e109c80811e99af9987
jezinka <jezina1989@gmail.com>
2025-06-17 18:48:30 +0200

    feat: update TransactionRow to display full description instead of truncated version

--- gui/src/components/transactionTable/TransactionRow.js
+++ gui/src/components/transactionTable/TransactionRow.js
@@ -8,7 +8,7 @@ const TransactionRow = ({transaction, editTransaction, splitTransaction, deleteT
     return (
         <tr key={transaction.id}>
             <td>{transaction.transactionDate}</td>
-            <td>{transaction.description.substring(0, 50)}</td>
+            <td>{transaction.description}</td>
             <td style={{textAlign: 'right'}}>{formatNumber(transaction.amount)}</td>
             <td style={{color: (transaction.categoryId === UNKNOWN_CATEGORY ? "lightgray" : "black")}}>{transaction.categoryName}</td>
             <td style={{textAlign: "center"}}>
